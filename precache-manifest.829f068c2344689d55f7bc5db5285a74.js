dd2f658fd2855b094be1bc9ee8fbb8d4c2f84d5a
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-05-16 08:52:02 +0300

    Add files via upload

---
+++ precache-manifest.829f068c2344689d55f7bc5db5285a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0f857f6354ab234fc873e04c01ab43",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "a2bf61c4c06c4af807f3",
+    "url": "/reacttodolist/static/css/main.83b70d63.chunk.css"
+  },
+  {
+    "revision": "12d92055d44b28575cde",
+    "url": "/reacttodolist/static/js/2.c7625123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.c7625123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bf61c4c06c4af807f3",
+    "url": "/reacttodolist/static/js/main.c7df4c8d.chunk.js"
+  },
+  {
+    "revision": "0bbb54b08aa002ac50ef",
+    "url": "/reacttodolist/static/js/runtime-main.150befda.js"
+  }
+]);
